83e07d25da85bd23809f8bf614918fbbef0d3ed6
vestaxmix <svensgertmanis@gmail.com>
2016-08-24 17:48:57 +0300

    first commit

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        void loginButton_Clicked(object sender,  EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+        void loginButton_Clicked2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
